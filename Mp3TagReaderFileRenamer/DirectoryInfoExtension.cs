ed950c472852997bca53c823154f04792b67e7e5
Haas, Dennis <Dennis.Haas@dertouristik.com>
2020-01-02 11:54:04 +0100

    commit

---
+++ Mp3TagReaderFileRenamer/DirectoryInfoExtension.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public static class DirectoryInfoExtension
+{
+    public static IEnumerable<string> DirectorySplit(this DirectoryInfo Dir)
+    {
+        while (Dir != null)
+        {
+            yield return Dir.Name;
+            Dir = Dir.Parent;
+        }
+    }
+
+    public static string DirectoryPart(this DirectoryInfo Dir, int PartNr)
+    {
+        string[] Parts = Dir.DirectorySplit().ToArray();
+        int L = Parts.Length;
+        return PartNr >= 0 && PartNr < L ? Parts[L - 1 - PartNr] : "";
+
+    }
+
+    public static int NumberOfParts(this DirectoryInfo Dir)
+    {
+        string[] Parts = Dir.DirectorySplit().ToArray();
+        return Parts.Length;
+
+
+    }
+
+    public static string[] Parts(this DirectoryInfo Dir)
+    {
+        string[] Parts = Dir.DirectorySplit().ToArray();
+        return Parts;
+    }
+}
---
+++ Mp3TagReaderFileRenamer/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace Mp3TagReaderFileRenamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderName = args[0];
+            if (args[0] == null || args[0].Equals("-?"))
+            {
+                Console.WriteLine("Mp3TagReaderFileRenamer  [folderName]");
+            }
+            string[] filePaths = Directory.GetFiles(folderName);
+            foreach (string file in filePaths)
+            {
+                uint trackno = ReadMp3Tags(file);
+                String newPath = AddTracknumber(trackno, file);
+                Console.WriteLine(newPath);
+                File.Copy(file, newPath);
+            }
+
+
+
+
+        }
+
+        private static String AddTracknumber(uint trackno, string file)
+        {
+            if (System.IO.File.Exists(file))
+            {
+                DirectoryInfo DI_Data = new DirectoryInfo(file);
+                int numberOfParts = DI_Data.NumberOfParts();
+                string filename = DI_Data.DirectoryPart(numberOfParts - 1);
+                string tracknoprefix = trackno / 10 == 0 ? "0" + trackno : "" + trackno;
+                filename = tracknoprefix + " - " + filename;
+                Console.WriteLine("???" + filename);
+                string newPath = "";
+                for (int i = DI_Data.NumberOfParts()-1 ; i > 0; i--)
+                {
+                    string suffix = DI_Data.Parts()[i];
+                    Console.WriteLine(suffix);
+                    if (DI_Data.NumberOfParts()- 1 == i)
+                    {
+                        newPath = newPath + suffix;
+                    }
+                    else
+                    {
+                        newPath = newPath + @"\" + suffix;
+                    }                
+                    Console.WriteLine("New Path" +newPath);
+                }
+                newPath = newPath + @"\" +filename;
+                newPath.Replace("\\\\", "\\");
+                Console.WriteLine("---" + file);
+                Console.WriteLine("***" + newPath);
+                return newPath;
+            }
+            return "";
+        }
+
+
+
+        private static uint ReadMp3Tags(string filename)
+        {
+            TagLib.File file = TagLib.File.Create(filename);
+            uint trackno = file.Tag.Track;
+            String title = file.Tag.Title;
+            String album = file.Tag.Album;
+            String length = file.Properties.Duration.ToString();
+            //Console.WriteLine(trackno);
+            //Console.WriteLine(title);
+            //Console.WriteLine(album);
+            //Console.WriteLine(length);
+            return trackno;
+        }
+    }
+}
